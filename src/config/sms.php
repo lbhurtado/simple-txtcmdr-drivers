e723fdac14101aedea056e21784539f5e177dd6d
Lester Hurtado <lester@applester.co>
2016-04-20 17:48:47 +0800

    Align arrows

--- src/config/sms.php
+++ src/config/sms.php
@@ -92,12 +92,12 @@ return [
         'verify' => true,
     ],
     'telerivet' => [
-        'api_key' => env('TELERIVET_API_KEY'),
+        'api_key'    => env('TELERIVET_API_KEY'),
         'project_id' => env('TELERIVET_PROJECT_ID'),
     ],
     'smart' => [
-        'token' => env('SMARTSUITE_TOKEN'),
-        'wsdl' => 'https://ws.smartmessaging.com.ph/soap/?wsdl',
+        'token'   => env('SMARTSUITE_TOKEN'),
+        'wsdl'    => 'https://ws.smartmessaging.com.ph/soap/?wsdl',
         'service' => 'SENDSMS',
     ],
 ];
